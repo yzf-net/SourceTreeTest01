be7e32966d2f90c42e804a5a4093a13b668ea696
yzf <865846263@qq.com>
2019-02-28 13:56:17 +0800

    新建项目:将文件加入到暂存区

---
+++ SourceTreeTest01/src/main/java/com/yzf/test/Hello_SourceTree.java
@@ -0,0 +1,11 @@
+package main.java.com.yzf.test;
+
+/**
+ * @author yzf
+ * @date 2019-02-28 13:53
+ */
+public class Hello_SourceTree {
+    public static void main(String[] args) {
+        System.out.println("Hello SourceTree");
+    }
+}
